76eabc34254449603bcb8f60a6bb741952bba518
gautte <s362066@oslomet.no>
2023-08-23 21:17:05 +0200

    Added a controller class: ItemController. Additionally changed the content of the controller and added some items

---
+++ MyShop/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Table()
+        {
+            var items = new List<Item>();
+            var item1 = new Item();
+            item1.ItemId = 1;
+            item1.Name = "Pizza";
+            item1.Price = 60;
+
+            var item2 = new Item()
+            {
+                ItemId = 2,
+                Name = "Fried Chicken Leg",
+                Price = 15
+            };
+
+            var item3 = new Item()
+            {
+                ItemId = 3,
+                Name = "Wok",
+                Price = 50
+            };
+
+            items.Add(item1);
+            items.Add(item2);
+            items.Add(item3);
+
+            ViewBag.CurrentViewName = "List of Shop Items";
+            return View(items);
+        }
+    }
+}
