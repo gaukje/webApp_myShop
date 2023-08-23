9fa90a73f8f79d2ad10b4a13dfb2ccbdbead559b
gautte <s362066@oslomet.no>
2023-08-23 20:45:20 +0200

    Changed the item class definition

--- MyShop/Models/Item.cs
+++ MyShop/Models/Item.cs
@@ -1,7 +1,13 @@
-﻿namespace MyShop.Models
+﻿using System.Runtime.CompilerServices;
+
+namespace MyShop.Models
 {
     public class Item()
     {
-
+        public int ItemId { get; set; }     // The member variables must begin with upper case letters
+        public String Name { get; set; } = string.Empty; // The strings and classes must be declared with default values (String.Empty)
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
