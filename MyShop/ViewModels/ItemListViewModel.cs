b65c66b7505981a50c9d6fbf6f88efdd3abb336b
gautte <s362066@oslomet.no>
2023-08-28 21:29:27 +0200

    Made  a ItemListViewModel folder and a class

---
+++ MyShop/ViewModels/ItemListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyShop.Models;
+
+namespace MyShop.ViewModels
+{
+    public class ItemListViewModel
+    {
+        public IEnumerable<Item> Items;
+        public String? CurrentViewName;
+
+        public ItemListViewModel(IEnumerable<Item> items, String? currentViewName)
+        {
+            Items = items;
+            CurrentViewName = currentViewName;
+        }
+    }
+}
