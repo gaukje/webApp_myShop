79cc9141ab48333cb65796cfe19c03dfe6a5b5bd
gautte <s362066@oslomet.no>
2023-09-02 14:36:38 +0200

    Added a comment to the ItemDbContext constructor to better understand its function

--- MyShop/Models/ItemDbContext.cs
+++ MyShop/Models/ItemDbContext.cs
@@ -5,8 +5,8 @@ namespace MyShop.Models;
 
 public class ItemDbContext : DbContext
 {
-    public ItemDbContext(DbContextOptions<ItemDbContext> options) : base(options)
-    {
+    public ItemDbContext(DbContextOptions<ItemDbContext> options) : base(options) //This line defines a class named 'ItemDbContext' that inherits from 'DbContext', which is a base class provided by Entity Framwork COre for creating a database context. A database context represents a session with the database, allowing you to query and manipulate data.
+    { //This is a constructor for the 'ItemDbContext' class. It takes an instance of 'DbContextOptions-ItemDbContext- as a parameter. This constructor is typically used to configure the database connection and options.
         Database.EnsureCreated();
     }
 
