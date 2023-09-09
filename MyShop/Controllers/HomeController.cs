c3d0adf84075136326c49dda6a0bee884f988a3e
gautte <s362066@oslomet.no>
2023-09-09 13:49:30 +0200

    Made some changes

--- MyShop/Controllers/HomeController.cs
+++ MyShop/Controllers/HomeController.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace MyShop.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
     }
 }
+
