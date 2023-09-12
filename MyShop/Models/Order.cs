e0f98b4d2390b49082431f495fc474d522897f9f
gautte <s362066@oslomet.no>
2023-09-12 19:52:11 +0200

    Added content to the classes

--- MyShop/Models/Order.cs
+++ MyShop/Models/Order.cs
@@ -2,5 +2,13 @@
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public String OrderDate { get; set; } = string.Empty;
+        public int CustomerId { get; set; }
+        //navigation property
+        public Customer Customer { get; set; } = default!;
+        //navigation property
+        public List<OrderItem>? OrderItems { get; set;}
+        public decimal TotalPrice { get; set; }
     }
 }
--- MyShop/Models/OrderItem.cs
+++ MyShop/Models/OrderItem.cs
@@ -2,5 +2,14 @@
 {
     public class OrderItem
     {
+        public int OrderItemId { get; set; }
+        public int ItemId { get; set; }
+        //navigation property
+        public Item Item { get; set; } = default!;
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+        //navigation property
+        public Order Order { get; set; } = default!;
+        public decimal OrderItemPrice { get; set; }
     }
 }
