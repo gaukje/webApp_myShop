bf4b1ad543da6122361a039dab4ca524ae454ed6
gautte <s362066@oslomet.no>
2023-09-12 19:56:33 +0200

    Added a navigation property in item.cs. Additionally added a comment to explain what this means

--- MyShop/Models/Item.cs
+++ MyShop/Models/Item.cs
@@ -9,6 +9,18 @@ namespace MyShop.Models
 		public decimal Price { get; set; }
 		public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+		//navigation property
+		public List<OrderItem>? OrderItems { get; set; }
 	}
 }
+
+/*Note: List<Order>? Orders is a navigation property in the Customer class.
+ * A navigation property in an entity class represents a relationship between 
+ * enitites and allows you to navigate from one enitity to related entities.
+ * In this case, the Orders property is a navigation property that represents
+ * the relationship between a Customer and their associated Order entities.
+ * By having the Orders property in the Customer class, you can easily
+ * access all the orders that belong to a specific customer.
+ * 
+ */
 
