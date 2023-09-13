7360b0d43656caa501790587887dfaa3ac17ebfb
gautte <s362066@oslomet.no>
2023-09-13 10:39:11 +0200

    Added comments to gain better understanding of the functionality

--- MyShop/Models/ItemDbContext.cs
+++ MyShop/Models/ItemDbContext.cs
@@ -21,3 +21,13 @@ public class ItemDbContext : DbContext
     }
 }
 
+
+/*By calling UseLazyLoadingProxies() in the OnConfiguring method, EF Core will generate
+ * proxies for your entity classes, allowing lazy loading of related entities when accessed.
+ * 
+ * With lazy loading enabled, when you access a navigation property like OrderItems of the Order class,
+ * EF Core will automatically load the related OrderItem entities from the database on-demand, without
+ * explicitly loading the entire tree of dependent objects connected by the navigation properties.
+ * 
+ * Consider using eager loading or explicit loading in performance-critical scenarios.
+ */
