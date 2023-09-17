51f9cc1d0e3caa0edbb77e0fab5a8ffbbf88ff8d
gautte <s362066@oslomet.no>
2023-09-17 15:52:18 +0200

    Modified the OrderController code

--- MyShop/Controllers/OrderController.cs
+++ MyShop/Controllers/OrderController.cs
@@ -1,6 +1,22 @@
-﻿namespace MyShop.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyShop.Controllers;
+
+public class OrderController : Controller
 {
-    public class OrderController
+    private readonly ItemDbContext _itemDbContext;
+
+    public OrderController(ItemDbContext itemDbContext)
     {
+        _itemDbContext = itemDbContext;
+    }
+
+    public async Task<IActionResult> Table()
+    {
+        List<Order> orders = await _itemDbContext.Orders.ToListAsync();
+        return View(orders);
     }
 }
+
