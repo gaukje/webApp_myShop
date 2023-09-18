e76f5d1d4177ce9da5c7df3bf0594c75d281f54d
gautte <s362066@oslomet.no>
2023-09-18 12:42:37 +0200

    Modified the IItemRepository

--- MyShop/DAL/IItemRepository.cs
+++ MyShop/DAL/IItemRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace MyShop.DAL
+﻿using MyShop.Models;
+
+namespace MyShop.DAL;
+
+public class IItemRepository
 {
-    public class IItemRepository
-    {
-    }
+    Task<IEnumerable<Item>> GetAll();
+    Task<Item?> GetItemById(int id);
+    Task Create(Item item);
+    Task Update(Item item);
+    Task<bool> Delete(int id);
 }
