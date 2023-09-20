5a0ac2cb7f9d78882b58255f1b9a263c206ef627
gautte <s362066@oslomet.no>
2023-09-20 14:45:18 +0200

    Modified the itemController for logging

--- MyShop/Controllers/ItemController.cs
+++ MyShop/Controllers/ItemController.cs
@@ -8,14 +8,19 @@ namespace MyShop.Controllers;
 public class ItemController : Controller
 {
     private readonly IItemRepository _itemRepository;
+    private readonly ILogger<ItemController> _logger;
 
-    public ItemController(IItemRepository itemRepository)
+    public ItemController(IItemRepository itemRepository, ILogger<ItemController> logger)
     {
         _itemRepository = itemRepository;
+        _logger = logger;
     }
 
     public async Task<IActionResult> Table()
     {
+        _logger.LogInformation("This is an information message.");
+        _logger.LogWarning("This is a warning message.");
+        _logger.LogError("This is an error message");
         var items = await _itemRepository.GetAll();
         var itemListViewModel = new ItemListViewModel(items, "Table");
         return View(itemListViewModel);
