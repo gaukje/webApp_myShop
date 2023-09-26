0fa3d283a289badc0a517afd69b81c4883e004ce
gautte <s362066@oslomet.no>
2023-09-26 17:22:33 +0200

    Added RegEx

--- MyShop/Models/Item.cs
+++ MyShop/Models/Item.cs
@@ -5,9 +5,18 @@ namespace MyShop.Models
     public class Item
     {
         public int ItemId { get; set; }
+
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}", ErrorMessage = "The Name must consist of numbers" +
+            " or letters and have a length between 2-20 characters.")]
+        [Display(Name = "Item name")]
         public string Name { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "The Price must be greater than 0.")]
         public decimal Price { get; set; }
+
+        [StringLength(200)]
         public string? Description { get; set; }
+
         public string? ImageUrl { get; set; }
         // navigation property
         public virtual List<OrderItem>? OrderItems { get; set; }
