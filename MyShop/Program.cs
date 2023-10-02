72d751d130c19028459b66f8abd8204e487db6a4
gautte <s362066@oslomet.no>
2023-10-02 12:25:23 +0200

    Modified the AddIdentity and AddSession methods

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -21,13 +21,39 @@ builder.Services.AddDbContext<ItemDbContext>(options => {
         builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddEntityFrameworkStores<ItemDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    //Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredUniqueChars = 6;
+
+    //Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    //User setting
+    options.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<ItemDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 
 builder.Services.AddRazorPages();
-builder.Services.AddSession();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".AdventureWorks.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(1800);
+    options.Cookie.IsEssential = true;
+});
 
 var loggerConfiguration = new LoggerConfiguration()
     .MinimumLevel.Information() // levels: Trace< Information < Warning < Error
