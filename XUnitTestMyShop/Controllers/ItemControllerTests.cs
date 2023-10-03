f0d6645e7be456352b3225aea258a3b394aa44f8
gautte <s362066@oslomet.no>
2023-10-03 22:23:17 +0200

    Added code to test the Create method in the ItemController

--- XUnitTestMyShop/Controllers/ItemControllerTests.cs
+++ XUnitTestMyShop/Controllers/ItemControllerTests.cs
@@ -24,7 +24,6 @@ public class ItemControllerTests
                 Description = "Delicious spicy chicken wing",
                 ImageUrl = "/images/chickenwing.jpg"
             },
-            
             new Item
             {
                 ItemId = 2,
@@ -47,5 +46,30 @@ public class ItemControllerTests
         var itemListViewModel = Assert.IsAssignableFrom<ItemListViewModel>(viewResult.ViewData.Model);
         Assert.Equal(2, itemListViewModel.Items.Count());
         Assert.Equal(itemList, itemListViewModel.Items);
+    }
+
+    [Fact]
+    public async Task TestCreateNotOk()
+    {
+        //Arrange
+        var testItem = new Item
+        {
+            ItemId = 1,
+            Price = 20,
+            Description = "Delicious spicy chicken wing",
+            ImageUrl = "/images/chickenwing.jpg"
+        };
+        var mockItemRepository = new Mock<IItemRepository>();
+        mockItemRepository.Setup(repo => repo.Create(testItem)).ReturnsAsync(false);
+        var mockLogger = new Mock<ILogger<ItemController>>();
+        var itemController = new ItemController(mockItemRepository.Object, mockLogger.Object);
+
+        //Act
+        var result = await itemController.Create(testItem);
+
+        //Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var viewItem = Assert.IsAssignableFrom<Item>(viewResult.ViewData.Model);
+        Assert.Equal(testItem, viewItem);
     }
 }
